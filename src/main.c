4f385ea9e69831cc7c05a8a5998f03d4e7bd0427
Evgeniy <evg54@internet.ru>
2023-10-14 22:39:25 +0700

    Parallel calc works

--- src/main.c
+++ src/main.c
@@ -8,12 +8,13 @@
 bool is_prime(long num); // проверка на простоту
 void generate_combinations_util(long x, long n, long current, long* combination, long index,
     long** result, long* num_combinations); // рекурсивная функция генерации комбинации простых чисел
-long** generate_combinations(long x, long n, long* num_combinations); // обертывает вызов рекурсивной функции
+long** generate_combinations(long x, long start, long end, long* num_combinations); // обертка для рекурсивной функции генерации
 void free_combinations(long** combinations, long num_combinations); // освобождает память
 long max(long *a, long sz); // поиск максимального значения
-long calc(long n, long x, long exp); // вычисление
 void run_seq(long n, long x, long exp); // запуск последовательного вычисления
 void run_par(long n, long x, long exp); // запуск параллельного вычисления
+long calc_seq(long n, long x, long exp); // последовательное вычисление
+long calc_par(long n, long x, long exp); // параллельное вычисление
 
 int main(int argc, char *argv[]) {
     long x; // количество чисел в комбинации
@@ -25,25 +26,39 @@ int main(int argc, char *argv[]) {
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
+
     if (rank == 0) {
     #endif
     printf("Enter N: ");
     if (!scanf("%ld", &n)) {
         printf("Input error\n");
+        #ifdef parallel
+        MPI_Finalize();
+        #endif
         return 0;
     }
     printf("Enter quantity of prime numbers: ");
     if (!scanf("%ld", &x)) {
         printf("Input error\n");
+        #ifdef parallel
+        MPI_Finalize();
+        #endif
         return 0;
     }
     printf("Enter exponent: ");
     if (!scanf("%ld", &exp)) {
         printf("Input error\n");
+        #ifdef parallel
+        MPI_Finalize();
+        #endif
         return 0;
     }
     #ifdef parallel
     }
+    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Bcast(&n, 1, MPI_LONG, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&x, 1, MPI_LONG, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&exp, 1, MPI_LONG, 0, MPI_COMM_WORLD);
     run_par(n, x, exp);
     MPI_Finalize();
     #endif
@@ -57,13 +72,25 @@ void run_seq(long n, long x, long exp) {
     long result = 0;
     double start, end;
     start = omp_get_wtime();
-    result = calc(n, x, exp);
+    result = calc_seq(n, x, exp);
     end = omp_get_wtime();
     printf("Seq: time = %.3lf sec, result = %ld\n", (end - start), result);
 }
 
 void run_par(long n, long x, long exp) {
-    
+    int rank, size;
+    long result = 0;
+    double start, end;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    if (rank == 0) {
+        start = omp_get_wtime();
+    }
+    result = calc_par(n, x, exp);
+    if (rank == 0) {
+        end = omp_get_wtime();
+        printf("Parallel: time = %.3lf sec, result = %ld\n", (end - start), result);
+    }
 }
 
 bool is_prime(long num) {
@@ -73,6 +100,29 @@ bool is_prime(long num) {
         if (num % i == 0) return false;
     }
     return true;
+}
+
+long** generate_combinations(long x, long start, long end, long* num_combinations) {
+    *num_combinations = 0;
+    long max_combinations = 1, i;
+    for (i = 0; i < x; i++) {
+        max_combinations *= (end - start - 1);
+    }
+
+    long** result = (long**)malloc(max_combinations * sizeof(long*));
+    if (result == NULL) {
+        fprintf(stderr, "Error allocate memory\n");
+        exit(EXIT_FAILURE);
+    }
+    for (i = 0; i < max_combinations; i++) {
+        result[i] = (long*)malloc(x * sizeof(long));
+    }
+
+    long* combination = (long*)malloc(x * sizeof(long));
+    generate_combinations_util(x, end - start, start, combination, start, result, num_combinations);
+    free(combination);
+
+    return result;
 }
 
 void generate_combinations_util(long x, long n, long current, long* combination, long index, long** result, long* num_combinations) {
@@ -90,29 +140,6 @@ void generate_combinations_util(long x, long n, long current, long* combination,
             generate_combinations_util(x, n, i, combination, index + 1, result, num_combinations);
         }
     }
-}
-
-long** generate_combinations(long x, long n, long* num_combinations) {
-    *num_combinations = 0;
-    long max_combinations = 1, i;
-    for (i = 0; i < x; i++) {
-        max_combinations *= (n - 1);
-    }
-
-    long** result = (long**)malloc(max_combinations * sizeof(long*));
-    if (result == NULL) {
-        fprintf(stderr, "Error allocate memory\n");
-        exit(EXIT_FAILURE);
-    }
-    for (i = 0; i < max_combinations; i++) {
-        result[i] = (long*)malloc(x * sizeof(long));
-    }
-
-    long* combination = (long*)malloc(x * sizeof(long));
-    generate_combinations_util(x, n, 2, combination, 0, result, num_combinations);
-    free(combination);
-
-    return result;
 }
 
 void free_combinations(long** combinations, long num_combinations) {
@@ -133,9 +160,9 @@ long max(long *a, long sz) {
     return m;
 }
 
-long calc(long n, long x, long exp) {
+long calc_seq(long n, long x, long exp) {
     long num_combinations, ans;
-    long** combinations = generate_combinations(x, n, &num_combinations);
+    long** combinations = generate_combinations(x, 0, n, &num_combinations);
     long* results = calloc(sizeof(long), num_combinations);
     long i;
     for (i = 0; i < num_combinations; i++) {
@@ -150,4 +177,30 @@ long calc(long n, long x, long exp) {
     free_combinations(combinations, num_combinations);
     free(results);
     return ans;
+}
+
+long calc_par(long n, long x, long exp) {
+    int rank, size;
+    long result;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    int chunk_size = n / size;
+    int start_index = rank * chunk_size;
+    int end_index = start_index + chunk_size;
+
+    long num_combinations;
+    long** combinations = generate_combinations_par(x, start_index, end_index, &num_combinations);
+    long* results = calloc(sizeof(long), num_combinations);
+    long i;
+    for (i = 0; i < num_combinations; i++) {
+        unsigned long j;
+        for (j = 0; j <= sizeof(combinations[i]) / sizeof(long); j++) {
+            results[i] += pow(combinations[i][j], exp);
+        }
+        if (results[i] >= n)
+            results[i] = 0;
+    }
+    result = max(results, num_combinations);
+    return result;
 }
